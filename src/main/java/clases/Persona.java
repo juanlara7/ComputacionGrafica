1e495db41c60536c9053bd76628c743bb15558f1
Administrador <Administrador@10.10.11.152>
2017-01-31 11:12:08 -0500

    se agrego y se modifico la escena con un webview

---
+++ src/main/java/clases/Persona.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Persona {
+    
+    private String Nombre;
+
+    /**
+     * Get the value of Nombre
+     *
+     * @return the value of Nombre
+     */
+    public String getNombre() {
+        return Nombre;
+    }
+
+    /**
+     * Set the value of Nombre
+     *
+     * @param Nombre new value of Nombre
+     */
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    private String Edad;
+
+    /**
+     * Get the value of Edad
+     *
+     * @return the value of Edad
+     */
+    public String getEdad() {
+        return Edad;
+    }
+
+    /**
+     * Set the value of Edad
+     *
+     * @param Edad new value of Edad
+     */
+    public void setEdad(String Edad) {
+        this.Edad = Edad;
+    }
+
+    private String Identificacion;
+
+    /**
+     * Get the value of Identificacion
+     *
+     * @return the value of Identificacion
+     */
+    public String getIdentificacion() {
+        return Identificacion;
+    }
+
+    /**
+     * Set the value of Identificacion
+     *
+     * @param Identificacion new value of Identificacion
+     */
+    public void setIdentificacion(String Identificacion) {
+        this.Identificacion = Identificacion;
+    }
+
+}
--- src/main/java/com/mycompany/pruebagit/FXMLController.java
+++ src/main/java/com/mycompany/pruebagit/FXMLController.java
@@ -6,17 +6,19 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.web.WebView;
 
 public class FXMLController implements Initializable {
     
     @FXML
     private Label label;
     
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
+    
+    
+        @FXML
+    private WebView webv;
+        
+    
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
